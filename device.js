88473d884938d9779371b46997f70d3e94f27b28
Maksimilian1984 <58360374+Maksimilian1984@users.noreply.github.com>
2020-06-24 22:08:49 +0300

    сжал img

--- device.js
+++ device.js
@@ -1,30 +1,51 @@
+
+
+
+
 const writeUs = document.querySelector('.write-us');
 const communicationForm = document.querySelector('.communication-form');
 const closeModal = document.querySelector('.close-modal');
 const closeMap = document.querySelector('.close-map');
 const smollMap = document.querySelector('.contacts img');
 const modalMap = document.querySelector('.modal-map');
-const nameClient = document.querySelector('[name = name-client]');
-const email = document.querySelector('[name=email]');
-const storage = localStorage.getItem('name');
+
 
 writeUs.addEventListener('click', (evt) => {
   evt.preventDefault();
   communicationForm.classList.add('vision');
-  if (storage) {
-    nameClient.value = storage;
-    email.focus();
-  } else {
-    nameClient.focus();
-  }
+
 });
 
-closeModal.onclick = function () {
+closeModal.onclick = () => {
   communicationForm.classList.remove('vision');
 };
-smollMap.onclick = function () {
+smollMap.onclick =  () => {
   modalMap.classList.add('vision');
 };
-closeMap.onclick = function () {
+closeMap.onclick = () => {
   modalMap.classList.remove('vision');
 };
+
+
+const radio1 = document.querySelector('#selfi');
+const radio2 = document.querySelector('#fitnes');
+const radio3 = document.querySelector('#cuadrocopter');
+
+var radioChecked = (button) => {
+  button.checked = true;
+  console.log(button)
+};
+
+
+var handlerSlider = () => {
+  if(radio1.checked) {
+    radioChecked(radio2)
+  } else if(radio2.checked) {
+    radioChecked(radio3)
+  } else if(radio3.checked) {
+    radioChecked(radio1)
+  }
+};
+
+
+setInterval(handlerSlider, 6000);
